d143dd1b99599e552061efc47b8b24e3f127523c
Tristan Cimaglia <tcimaglia20@gmail.com>
2026-08-05 13:45:54 -0700

    Fixed Header and added Our Heritage

--- features/heritage/heritage.routes.js
+++ features/heritage/heritage.routes.js
@@ -5,4 +5,12 @@ router.get('/legacy', (req, res) => {
     res.render('legacy', { title: 'Our Heritage' });
 });
 
+router.get('/profiles/pietro-cimaglia', (req, res) => {
+    res.render('profiles/pietro-cimaglia', { title: 'Pietro Cimaglia' });
+});
+
+router.get('/vault', (req, res) => {
+    res.render('vault', { title: 'Vault' })
+});
+
 module.exports = router;
